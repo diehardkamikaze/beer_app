4d92b44e5de276fb5226481b00f4d175982384cb
diehardkamikaze <diehardkamikaze@gmail.com>
2021-05-03 23:38:03 +0300

    Added new Background functionality

---
+++ src/interface/Background.js
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { IMAGES_COUNT } from '../constants';
+
+class Background extends React.Component {
+  constructor(props) {
+    super(props);
+    this.timer = -1;
+    this.state = { count: 0 };
+    this.changeBg = this.changeBg.bind(this);
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(this.changeBg, 2500);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  changeBg() {
+    this.setState((state) => ({ count: (state.count + 1) % IMAGES_COUNT }));
+  }
+
+  render() {
+    const { images } = this.props;
+    const { count } = this.state;
+    return <img src={images[count]} className="background" alt="NICE" />;
+  }
+}
+
+export default Background;
