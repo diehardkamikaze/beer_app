013e78d2c631e810341193f86a04d743a41c06b8
diehardkamikaze <diehardkamikaze@gmail.com>
2021-05-03 23:41:14 +0300

    Some struct changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
-import Catalog from './catalog.js'
+import './index.css';
+import App from './App';
 
-fetch('https://api.punkapi.com/v2/beers')
-.then((result) => result.json())
-.then((responce) => {
-  console.log(responce);
-  ReactDOM.render(
-    <Catalog items={responce}></Catalog>,
-    document.getElementById('root')
-  );
-});
-
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
