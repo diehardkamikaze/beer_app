4414eeb2bf51cad50c66f4b42efe5f3b69896361
diehardkamikaze <diehardkamikaze@gmail.com>
2021-05-03 23:38:31 +0300

    Started Registration functionality

---
+++ src/interface/Registration.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class RegistrationModal extends React.Component {
+  render() {
+    return (
+      <div className="regModal">TEST</div>
+    );
+  }
+}
+
+export default RegistrationModal;
