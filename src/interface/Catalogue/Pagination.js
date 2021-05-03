dd3d98a0c816019178c7f9e0a0369d00065edc00
diehardkamikaze <diehardkamikaze@gmail.com>
2021-05-03 23:38:58 +0300

    Added pagination functionality

---
+++ src/interface/Catalogue/Pagination.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Pagination extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick({ target }) {
+    // eslint-disable-next-line react/prop-types
+    const { pagi, handlePagi } = this.props;
+    if (target.id === 'prev' && pagi === 1) return;
+    handlePagi(target.id);
+  }
+
+  render() {
+    // eslint-disable-next-line react/prop-types
+    const { pagi } = this.props;
+    return (
+      <div className="pagination">
+        <button onClick={this.onClick} id="prev" type="button">&lt;&lt;</button>
+        <span>{pagi}</span>
+        <button onClick={this.onClick} id="next" type="button">&gt;&gt;</button>
+      </div>
+    );
+  }
+}
+
+export default Pagination;
